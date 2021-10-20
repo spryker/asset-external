fc7b36279db927bee629b8ce7d1afe16ff14e113
TimurSultanov <timur.sultanov@spryker.com>
2021-10-20 14:41:00 +0200

    APPS-1947: add test cases for storage listeners, removed persistence test suite

--- tests/SprykerTest/Shared/AssetExternal/_support/Helper/AssetExternalDataHelper.php
+++ tests/SprykerTest/Shared/AssetExternal/_support/Helper/AssetExternalDataHelper.php
@@ -26,9 +26,19 @@ class AssetExternalDataHelper extends Module
      *
      * @return \Generated\Shared\Transfer\AssetExternalTransfer
      */
+    public function haveAssetExternalTransfer(array $seed = []): AssetExternalTransfer
+    {
+        return (new AssetExternalBuilder($seed))->build();
+    }
+
+    /**
+     * @param array $seed
+     *
+     * @return \Generated\Shared\Transfer\AssetExternalTransfer
+     */
     public function haveAssetExternal(array $seed = []): AssetExternalTransfer
     {
-        $assetExternalTransfer = (new AssetExternalBuilder($seed))->build();
+        $assetExternalTransfer = $this->haveAssetExternalTransfer($seed);
 
         $assetExternalEntity = SpyAssetExternalQuery::create()
             ->filterByAssetUuid($assetExternalTransfer->getAssetUuid())
@@ -74,43 +84,5 @@ class AssetExternalDataHelper extends Module
         $this->getDataCleanupHelper()->_addCleanup(function () use ($assetExternalStoreEntity): void {
             $assetExternalStoreEntity->delete();
         });
-    }
-
-    /**
-     * @param \Generated\Shared\Transfer\AssetExternalTransfer $assetExternalTransfer
-     *
-     * @return void
-     */
-    public function deleteAssetExternal(AssetExternalTransfer $assetExternalTransfer): void
-    {
-        SpyAssetExternalStoreQuery::create()
-            ->findByFkAssetExternal($assetExternalTransfer->getIdAssetExternalOrFail())
-            ->delete();
-
-        $assetExternalEntity = SpyAssetExternalQuery::create()
-            ->findOneByIdAssetExternal($assetExternalTransfer->getIdAssetExternalOrFail());
-
-        if ($assetExternalEntity !== null) {
-            $assetExternalEntity->delete();
-        }
-    }
-
-    /**
-     * @param \Generated\Shared\Transfer\AssetExternalTransfer $assetExternalTransfer
-     *
-     * @return \Generated\Shared\Transfer\AssetExternalTransfer
-     */
-    public function updateAssetExternal(AssetExternalTransfer $assetExternalTransfer): AssetExternalTransfer
-    {
-        $assetExternalEntity = SpyAssetExternalQuery::create()
-            ->filterByAssetUuid($assetExternalTransfer->getAssetUuid())
-            ->findOneOrCreate();
-
-        $assetExternalEntity->fromArray($assetExternalTransfer->toArray());
-        $assetExternalEntity->save();
-
-        $assetExternalTransfer->setIdAssetExternal($assetExternalEntity->getIdAssetExternal());
-
-        return $assetExternalTransfer;
     }
 }
